de7e7b10aec49a60832cde68915ccae1433bb944
Noah8863 <stacknoah99@gmail.com>
2022-03-26 20:56:08 -0600

    Starting over and going back to 1 day schedule

--- scheduler.js
+++ scheduler.js
@@ -1,91 +1,47 @@
-let test = document.getElementById("Monday")
-let test1 = document.getElementById("Tuesday")
-let test2 = document.getElementById("Wednsday")
-let test3 = document.getElementById("Thursday")
-let test4 = document.getElementById("Friday")
+let mondayNewValue = document.getElementById("newMondayBox")
+let tuesdayNewValue = document.getElementById("newTuesdayBox")
+let wednsdayNewValue = document.getElementById("newWednsdayBox")
+let thursdayNewValue = document.getElementById("newThursdayBox")
+let fridayNewValue = document.getElementById("newFridayBox")
+
+//Event Listeners for all the times/days
+//This is over kill and I bit off more than I can chew
+let nineMondayBox = document.getElementById("9amMondayBox")
+let tenMondayBox = document.getElementById("10amMondayBox")
+let elevenMondayBox = document.getElementById("11amMondayBox")
+let twelveMondayBox = document.getElementById("12pmMondayBox")
+let oneMondayBox = document.getElementById("1pmMondayBox")
+let twoMondayBox = document.getElementById("2pmMondayBox")
+let threeMondayBox = document.getElementById("3pmMondayBox")
+let fourMondayBox = document.getElementById("4pmMondayBox")
+let fiveMondayBox = document.getElementById("5pmMondayBox")
+
+let time = document.getElementById("currentTimeElement")
 
 
+//Submission and Reset Buttons Go Here
+var resetBtnElement = document.getElementById('resetBtn')
+var submitButton = document.getElementById('saveBtn')
+
+//Event Listeners for those Buttons
+submitButton.addEventListener('click', addEvents)
+resetBtnElement.addEventListener('click', resetEvents)
 
 //This is the time element
 var currentTime = function() {
-    document.getElementById("currentTimeElement").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-}
+    time.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+    
+} 
 setInterval(currentTime, 1000);
 
-//This is the submission button
-var submitButton = document.getElementById('saveBtn')
-submitButton.addEventListener('click', getInputFromUser)
-
-
-//This needs to be worked on
-function setTime(){
-    if (currentTime === moment().format("MMMM Do YYYY, 9:01:30 0")){
-        test.setAttribute('class', 'present')
-    }
-}
-
-function getInputFromUser(){
-    mondayNewValue = test.value
-    if (test && mondayNewValue) {
-        beep = localStorage.setItem('testing', mondayNewValue)
-        console.log(mondayNewValue)
-    } else {
-        console.log('nothing here for monday')
-    }
-    tuesdayNewValue = test1.value
-    if (test1 && tuesdayNewValue) {
-        console.log(tuesdayNewValue)
-    } else {
-        console.log('nothing here for tuesday')
-    }
-    wednsdayNewValue = test2.value
-    if (test2 && wednsdayNewValue) {
-        console.log(wednsdayNewValue)
-    } else {
-        console.log('nothing here for wedsnday')
-    }
-    thursdayNewValue = test3.value
-    if (test3 && thursdayNewValue) {
-        console.log(thursdayNewValue)
-    } else {
-        console.log('nothing here for thursday')
-    }
-    fridayNewValue = test4.value
-    if (test4 && fridayNewValue) {
-        console.log(fridayNewValue)
-    } else {
-        console.log ('nothing here for friday')
-    }
+function addEvents(){
     
 }
 
-function newValues (){
-    test.setAttribute('value', 'beep')
-}
 
 
-window.onload = function newValues() {
-    var reloading = localStorage.getItem('beep');
-    if (reloading) {
-        sessionStorage.removeItem("testing");
-    }
-}
-
-function reloadP() {
-    sessionStorage.setItem("reloading", "true");
-    document.location.reload();
-}
-
-
-
-
-
-
-
-
-
-//figure out how to link time with functions for this to work
-function timeRest () {
+//reset Events
+function resetEvents () {
+    //depending on the day, wipe this out
     localStorage.clear()
 }
-
