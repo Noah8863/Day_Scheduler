ff3954c2f86a65c8f559555c49d639d79825b610
Noah8863 <stacknoah99@gmail.com>
2022-03-26 22:00:37 -0600

    Fixing Issues

--- scheduler.js
+++ scheduler.js
@@ -1,23 +1,13 @@
-let mondayNewValue = document.getElementById("newMondayBox")
-let tuesdayNewValue = document.getElementById("newTuesdayBox")
-let wednsdayNewValue = document.getElementById("newWednsdayBox")
-let thursdayNewValue = document.getElementById("newThursdayBox")
-let fridayNewValue = document.getElementById("newFridayBox")
-
-//Event Listeners for all the times/days
-//This is over kill and I bit off more than I can chew
-let nineMondayBox = document.getElementById("9amMondayBox")
-let tenMondayBox = document.getElementById("10amMondayBox")
-let elevenMondayBox = document.getElementById("11amMondayBox")
-let twelveMondayBox = document.getElementById("12pmMondayBox")
-let oneMondayBox = document.getElementById("1pmMondayBox")
-let twoMondayBox = document.getElementById("2pmMondayBox")
-let threeMondayBox = document.getElementById("3pmMondayBox")
-let fourMondayBox = document.getElementById("4pmMondayBox")
-let fiveMondayBox = document.getElementById("5pmMondayBox")
-
+var nineMondayBox = document.getElementById("9amMondayBox")
+var tenMondayBox = document.getElementById("10amMondayBox")
+var elevenMondayBox = document.getElementById("11amMondayBox")
+var twelveMondayBox = document.getElementById("12pmMondayBox")
+var oneMondayBox = document.getElementById("1pmMondayBox")
+var twoMondayBox = document.getElementById("2pmMondayBox")
+var threeMondayBox = document.getElementById("3pmMondayBox")
+var fourMondayBox = document.getElementById("4pmMondayBox")
+var fiveMondayBox = document.getElementById("5pmMondayBox")
 let time = document.getElementById("currentTimeElement")
-
 
 //Submission and Reset Buttons Go Here
 var resetBtnElement = document.getElementById('resetBtn')
@@ -34,9 +24,44 @@ var currentTime = function() {
 } 
 setInterval(currentTime, 1000);
 
+
+//Setting variable that will hold Mondays Storage
+
+var mondayNineAmStorage = localStorage.getItem("9amMondayStorage")
+var mondayTenAmStorage  = localStorage.getItem("10amMondayStorage") 
+var mondayElevenAmStoreage  = localStorage.getItem("11amMondayStorage") 
+var mondayTwelvePmStorage  = localStorage.getItem("12pmMondayStorage") 
+var mondayOnePmStorage  = localStorage.getItem("1pmMondayStorage") 
+var mondayTwoPmStorage  = localStorage.getItem("2pmMondayStorage") 
+var mondayThreePmStorage  = localStorage.getItem("3pmMondayStorage") 
+var mondayFourPmStorage  = localStorage.getItem("4pmMondayStorage") 
+var mondayFivePmStorage  = localStorage.getItem("5pmMondayStorage") 
+
+
+// Setting input variables text contents to new variables
+
+nineMondayBox.textContent = mondayNineAmStorage
+tenMondayBox.textContent = mondayTenAmStorage
+elevenMondayBox.textContent = mondayElevenAmStoreage
+twelveMondayBox.textContent = mondayTwelvePmStorage
+oneMondayBox.textContent = mondayOnePmStorage
+twoMondayBox.textContent = mondayTwoPmStorage
+threeMondayBox.textContent = mondayThreePmStorage
+fourMondayBox.textContent = mondayFourPmStorage
+fiveMondayBox.textContent = mondayFivePmStorage
+
 function addEvents(){
-    
+    localStorage.setItem("9amMondayStorage", nineMondayBox.value)
+    localStorage.setItem("10amMondayStorage", tenMondayBox.value)
+    localStorage.setItem("11amMondayStorage", elevenMondayBox.value)
+    localStorage.setItem("12pmMondayStorage", twelveMondayBox.value)
+    localStorage.setItem("1pmMondayStorage", oneMondayBox.value)
+    localStorage.setItem("2pmMondayStorage", twoMondayBox.value)
+    localStorage.setItem("3pmMondayStorage", threeMondayBox.value)
+    localStorage.setItem("4pmMondayStorage", fourMondayBox.value)
+    localStorage.setItem("5pmMondayStorage", fiveMondayBox.value)
 }
+
 
 
 
@@ -44,4 +69,5 @@ function addEvents(){
 function resetEvents () {
     //depending on the day, wipe this out
     localStorage.clear()
+    window.location.reload();
 }
