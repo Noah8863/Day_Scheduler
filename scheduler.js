33f34743ce531fd51f8db4badedd27eea47e121d
Noah8863 <stacknoah99@gmail.com>
2022-03-26 22:24:04 -0600

    Added Tuesday

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,4 @@
+//Monday Elements
 var nineMondayBox = document.getElementById("9amMondayBox")
 var tenMondayBox = document.getElementById("10amMondayBox")
 var elevenMondayBox = document.getElementById("11amMondayBox")
@@ -7,6 +8,18 @@ var twoMondayBox = document.getElementById("2pmMondayBox")
 var threeMondayBox = document.getElementById("3pmMondayBox")
 var fourMondayBox = document.getElementById("4pmMondayBox")
 var fiveMondayBox = document.getElementById("5pmMondayBox")
+//Tuesday Elements
+var nineTuesdayBox = document.getElementById("9amTuesdayBox")
+var tenTuesdayBox = document.getElementById("10amTuesdayBox")
+var elevenTuesdayBox = document.getElementById("11amTuesdayBox")
+var twelveTuesdayBox = document.getElementById("12pmTuesdayBox")
+var oneTuesdayBox = document.getElementById("1pmTuesdayBox")
+var twoTuesdayBox = document.getElementById("2pmTuesdayBox")
+var threeTuesdayBox = document.getElementById("3pmTuesdayBox")
+var fourTuesdayBox = document.getElementById("4pmTuesdayBox")
+var fiveTuesdayBox = document.getElementById("5pmTuesdayBox")
+
+
 let time = document.getElementById("currentTimeElement")
 
 //Submission and Reset Buttons Go Here
@@ -22,8 +35,15 @@ var currentTime = function() {
     time.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
     
 } 
+
 setInterval(currentTime, 1000);
 
+function updatingEvent(){
+    var grabbingHourforMonday = (time.textContent.split(',')[1].split(':')[0].trim())
+    if (grabbingHourforMonday === moment().format("9")){
+        mondayNewValue.setAttribute('class', 'present')
+    }
+}
 
 //Setting variable that will hold Mondays Storage
 
@@ -37,6 +57,16 @@ var mondayThreePmStorage  = localStorage.getItem("3pmMondayStorage")
 var mondayFourPmStorage  = localStorage.getItem("4pmMondayStorage") 
 var mondayFivePmStorage  = localStorage.getItem("5pmMondayStorage") 
 
+//Setting variable that will hold Tuesday Storage
+var tuesdayNineAmStorage = localStorage.getItem("9amTuesdayStorage")
+var tuesdayTenAmStorage  = localStorage.getItem("10amTuesdayStorage") 
+var tuesdayElevenAmStoreage  = localStorage.getItem("11amTuesdayStorage") 
+var tuesdayTwelvePmStorage  = localStorage.getItem("12pmTuesdayStorage") 
+var tuesdayOnePmStorage  = localStorage.getItem("1pmTuesdayStorage") 
+var tuesdayTwoPmStorage  = localStorage.getItem("2pmTuesdayStorage") 
+var tuesdayThreePmStorage  = localStorage.getItem("3pmTuesdayStorage") 
+var tuesdayFourPmStorage  = localStorage.getItem("4pmTuesdayStorage") 
+var tuesdayFivePmStorage  = localStorage.getItem("5pmTuesdayStorage")
 
 // Setting input variables text contents to new variables
 
@@ -50,6 +80,16 @@ threeMondayBox.textContent = mondayThreePmStorage
 fourMondayBox.textContent = mondayFourPmStorage
 fiveMondayBox.textContent = mondayFivePmStorage
 
+nineTuesdayBox.textContent = tuesdayNineAmStorage
+tenTuesdayBox.textContent = tuesdayTenAmStorage
+elevenTuesdayBox.textContent = tuesdayElevenAmStoreage
+twelveTuesdayBox.textContent = tuesdayTwelvePmStorage
+oneTuesdayBox.textContent = tuesdayOnePmStorage
+twoTuesdayBox.textContent = tuesdayTwoPmStorage
+threeTuesdayBox.textContent = tuesdayThreePmStorage
+fourTuesdayBox.textContent = tuesdayFourPmStorage
+fiveTuesdayBox.textContent = tuesdayFivePmStorage
+
 function addEvents(){
     localStorage.setItem("9amMondayStorage", nineMondayBox.value)
     localStorage.setItem("10amMondayStorage", tenMondayBox.value)
@@ -60,6 +100,16 @@ function addEvents(){
     localStorage.setItem("3pmMondayStorage", threeMondayBox.value)
     localStorage.setItem("4pmMondayStorage", fourMondayBox.value)
     localStorage.setItem("5pmMondayStorage", fiveMondayBox.value)
+
+    localStorage.setItem("9amTuesdayStorage", nineTuesdayBox.value)
+    localStorage.setItem("10amTuesdayStorage", tenTuesdayBox.value)
+    localStorage.setItem("11amTuesdayStorage", elevenTuesdayBox.value)
+    localStorage.setItem("12pmTuesdayStorage", twelveTuesdayBox.value)
+    localStorage.setItem("1pmTuesdayStorage", oneTuesdayBox.value)
+    localStorage.setItem("2pmTuesdayStorage", twoTuesdayBox.value)
+    localStorage.setItem("3pmTuesdayStorage", threeTuesdayBox.value)
+    localStorage.setItem("4pmTuesdayStorage", fourTuesdayBox.value)
+    localStorage.setItem("5pmTuesdayStorage", fiveTuesdayBox.value)
 }
 
 
