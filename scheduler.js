475b9256f51b7964e9043d96b243f08bbacba4c3
Noah8863 <stacknoah99@gmail.com>
2022-06-25 13:45:25 -0600

    trouble shooting

--- scheduler.js
+++ scheduler.js
@@ -18,7 +18,36 @@ var twoTuesdayBox = document.getElementById("2pmTuesdayBox")
 var threeTuesdayBox = document.getElementById("3pmTuesdayBox")
 var fourTuesdayBox = document.getElementById("4pmTuesdayBox")
 var fiveTuesdayBox = document.getElementById("5pmTuesdayBox")
-
+//Wednsday Elements
+var nineWedsndayBox = document.getElementById("9amWedsndayBox")
+var tenWedsndayBox = document.getElementById("10amWedsndayBox")
+var elevenWedsndayBox = document.getElementById("11amWedsndayBox")
+var twelveWedsndayBox = document.getElementById("12pmWedsndayBox")
+var oneWedsndayBox = document.getElementById("1pmWedsndayBox")
+var twoWedsndayBox = document.getElementById("2pmWedsndayBox")
+var threeWedsndayBox = document.getElementById("3pmWedsndayBox")
+var fourWedsndayBox = document.getElementById("4pmWedsndayBox")
+var fiveWedsndayBox = document.getElementById("5pmWedsndayBox")
+//Thursday Elements
+var nineThursdayBox = document.getElementById("9amThursdayBox")
+var tenThursdayBox = document.getElementById("10amThursdayBox")
+var elevenThursdayBox = document.getElementById("11amThursdayBox")
+var twelveThursdayBox = document.getElementById("12pmThursdayBox")
+var oneThursdayBox = document.getElementById("1pmThursdayBox")
+var twoThursdayBox = document.getElementById("2pmThursdayBox")
+var threeThursdayBox = document.getElementById("3pmThursdayBox")
+var fourThursdayBox = document.getElementById("4pmThursdayBox")
+var fiveThursdayBox = document.getElementById("5pmThursdayBox")
+//Friay Elements
+var nineFridayBox = document.getElementById("9amFridayBox")
+var tenFridayBox = document.getElementById("10amFridayBox")
+var elevenFridayBox = document.getElementById("11amFridayBox")
+var twelveFridayBox = document.getElementById("12pmFridayBox")
+var oneFridayBox = document.getElementById("1pmFridayBox")
+var twoFridayBox = document.getElementById("2pmFridayBox")
+var threeFridayBox = document.getElementById("3pmFridayBox")
+var fourFridayBox = document.getElementById("4pmFridayBox")
+var fiveFridayBox = document.getElementById("5pmFridayBox")
 
 let time = document.getElementById("currentTimeElement")
 
@@ -44,9 +73,8 @@ function updatingEvent(){
         mondayNewValue.setAttribute('class', 'present')
     }
 }
-
+//Trying to see if the commits will work
 //Setting variable that will hold Mondays Storage
-
 var mondayNineAmStorage = localStorage.getItem("9amMondayStorage")
 var mondayTenAmStorage  = localStorage.getItem("10amMondayStorage") 
 var mondayElevenAmStoreage  = localStorage.getItem("11amMondayStorage") 
@@ -56,7 +84,6 @@ var mondayTwoPmStorage  = localStorage.getItem("2pmMondayStorage")
 var mondayThreePmStorage  = localStorage.getItem("3pmMondayStorage") 
 var mondayFourPmStorage  = localStorage.getItem("4pmMondayStorage") 
 var mondayFivePmStorage  = localStorage.getItem("5pmMondayStorage") 
-
 //Setting variable that will hold Tuesday Storage
 var tuesdayNineAmStorage = localStorage.getItem("9amTuesdayStorage")
 var tuesdayTenAmStorage  = localStorage.getItem("10amTuesdayStorage") 
@@ -67,6 +94,36 @@ var tuesdayTwoPmStorage  = localStorage.getItem("2pmTuesdayStorage")
 var tuesdayThreePmStorage  = localStorage.getItem("3pmTuesdayStorage") 
 var tuesdayFourPmStorage  = localStorage.getItem("4pmTuesdayStorage") 
 var tuesdayFivePmStorage  = localStorage.getItem("5pmTuesdayStorage")
+//Setting variable that will hold Wedsnday Storage
+var wedsndayNineAmStorage = localStorage.getItem("9amWedsndayStorage")
+var wedsndayTenAmStorage  = localStorage.getItem("10amWedsndayStorage") 
+var wedsndayElevenAmStoreage  = localStorage.getItem("11amWedsndayStorage") 
+var wedsndayTwelvePmStorage  = localStorage.getItem("12pmWedsndayStorage") 
+var wedsndayOnePmStorage  = localStorage.getItem("1pmWedsndayStorage") 
+var wedsndayTwoPmStorage  = localStorage.getItem("2pmWedsndayStorage") 
+var wedsndayThreePmStorage  = localStorage.getItem("3pmWedsndayStorage") 
+var wedsndayFourPmStorage  = localStorage.getItem("4pmWedsndayStorage") 
+var wedsndayFivePmStorage  = localStorage.getItem("5pmWedsndayStorage")
+//Setting variables that will hold Thursday Storage
+var thursdayNineAmStorage = localStorage.getItem("9amThursdayStorage")
+var thursdayTenAmStorage  = localStorage.getItem("10amThursdayStorage") 
+var thursdayElevenAmStoreage  = localStorage.getItem("11amThursdayStorage") 
+var thursdayTwelvePmStorage  = localStorage.getItem("12pmThursdayStorage") 
+var thursdayOnePmStorage  = localStorage.getItem("1pmThursdayStorage") 
+var thursdayTwoPmStorage  = localStorage.getItem("2pmThursdayStorage") 
+var thursdayThreePmStorage  = localStorage.getItem("3pmThursdayStorage") 
+var thursdayFourPmStorage  = localStorage.getItem("4pmThursdayStorage") 
+var thursdayFivePmStorage  = localStorage.getItem("5pmThursdayStorage")
+//Setting variables that will hold Friday Storage
+var fridayNineAmStorage = localStorage.getItem("9amFridayStorage")
+var fridayTenAmStorage  = localStorage.getItem("10amFridayStorage") 
+var fridayElevenAmStoreage  = localStorage.getItem("11amFridayStorage") 
+var fridayTwelvePmStorage  = localStorage.getItem("12pmFridayStorage") 
+var fridayOnePmStorage  = localStorage.getItem("1pmFridayStorage") 
+var fridayTwoPmStorage  = localStorage.getItem("2pmFridayStorage") 
+var fridayThreePmStorage  = localStorage.getItem("3pmFridayStorage") 
+var fridayFourPmStorage  = localStorage.getItem("4pmFridayStorage") 
+var fridayFivePmStorage  = localStorage.getItem("5pmFridayStorage")
 
 // Setting input variables text contents to new variables
 
@@ -90,6 +147,36 @@ threeTuesdayBox.textContent = tuesdayThreePmStorage
 fourTuesdayBox.textContent = tuesdayFourPmStorage
 fiveTuesdayBox.textContent = tuesdayFivePmStorage
 
+nineWedsndayBox.textContent = wedsndayNineAmStorage
+tenWedsndayBox.textContent = wedsndayTenAmStorage
+elevenWedsndayBox.textContent = wedsndayElevenAmStoreage
+twelveWedsndayBox.textContent = wedsndayTwelvePmStorage
+oneWedsndayBox.textContent = wedsndayOnePmStorage
+twoWedsndayBox.textContent = wedsndayTwoPmStorage
+threeWedsndayBox.textContent = wedsndayThreePmStorage
+fourWedsndayBox.textContent = wedsndayFourPmStorage
+fiveWedsndayBox.textContent = wedsndayFivePmStorage
+
+nineThursdayBox.textContent = thursdayNineAmStorage
+tenThursdayBox.textContent = thursdayTenAmStorage
+elevenThursdayBox.textContent = thursdayElevenAmStoreage
+twelveThursdayBox.textContent = thursdayTwelvePmStorage
+oneThursdayBox.textContent = thursdayOnePmStorage
+twoThursdayBox.textContent = thursdayTwoPmStorage
+threeThursdayBox.textContent = thursdayThreePmStorage
+fourThursdayBox.textContent = thursdayFourPmStorage
+fiveThursdayBox.textContent = thursdayFivePmStorage
+
+nineFridayBox.textContent = fridayNineAmStorage
+tenFridayBox.textContent = fridayTenAmStorage
+elevenFridayBox.textContent = fridayElevenAmStoreage
+twelveFridayBox.textContent = fridayTwelvePmStorage
+oneFridayBox.textContent = fridayOnePmStorage
+twoFridayBox.textContent = fridayTwoPmStorage
+threeFridayBox.textContent = fridayThreePmStorage
+fourFridayBox.textContent = fridayFourPmStorage
+fiveFridayBox.textContent = fridayFivePmStorage
+
 function addEvents(){
     localStorage.setItem("9amMondayStorage", nineMondayBox.value)
     localStorage.setItem("10amMondayStorage", tenMondayBox.value)
@@ -110,6 +197,36 @@ function addEvents(){
     localStorage.setItem("3pmTuesdayStorage", threeTuesdayBox.value)
     localStorage.setItem("4pmTuesdayStorage", fourTuesdayBox.value)
     localStorage.setItem("5pmTuesdayStorage", fiveTuesdayBox.value)
+
+    localStorage.setItem("9amWedsndayStorage", nineWedsndayBox.value)
+    localStorage.setItem("10amWedsndayStorage", tenWedsndayBox.value)
+    localStorage.setItem("11amWedsndayStorage", elevenWedsndayBox.value)
+    localStorage.setItem("12pmWedsndayStorage", twelveWedsndayBox.value)
+    localStorage.setItem("1pmWedsndayStorage", oneWedsndayBox.value)
+    localStorage.setItem("2pmWedsndayStorage", twoWedsndayBox.value)
+    localStorage.setItem("3pmWedsndayStorage", threeWedsndayBox.value)
+    localStorage.setItem("4pmWedsndayStorage", fourWedsndayBox.value)
+    localStorage.setItem("5pmWedsndayStorage", fiveWedsndayBox.value)
+
+    localStorage.setItem("9amThursdayStorage", nineThursdayBox.value)
+    localStorage.setItem("10amThursdayStorage", tenThursdayBox.value)
+    localStorage.setItem("11amThursdayStorage", elevenThursdayBox.value)
+    localStorage.setItem("12pmThursdayStorage", twelveThursdayBox.value)
+    localStorage.setItem("1pmThursdayStorage", oneThursdayBox.value)
+    localStorage.setItem("2pmThursdayStorage", twoThursdayBox.value)
+    localStorage.setItem("3pmThursdayStorage", threeThursdayBox.value)
+    localStorage.setItem("4pmThursdayStorage", fourThursdayBox.value)
+    localStorage.setItem("5pmThursdayStorage", fiveThursdayBox.value)
+
+    localStorage.setItem("9amFridayStorage", nineFridayBox.value)
+    localStorage.setItem("10amFridayStorage", tenFridayBox.value)
+    localStorage.setItem("11amFridayStorage", elevenFridayBox.value)
+    localStorage.setItem("12pmFridayStorage", twelveFridayBox.value)
+    localStorage.setItem("1pmFridayStorage", oneFridayBox.value)
+    localStorage.setItem("2pmFridayStorage", twoFridayBox.value)
+    localStorage.setItem("3pmFridayStorage", threeFridayBox.value)
+    localStorage.setItem("4pmFridayStorage", fourFridayBox.value)
+    localStorage.setItem("5pmFridayStorage", fiveFridayBox.value)
 }
 
 
